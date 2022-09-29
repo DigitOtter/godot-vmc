a0e6c821cb44059ca0935526e4c39da2499a621c
DigitOtter <digitotter@protonmail.com>
2022-09-29 18:37:26 +0000

    Update gdscripts to Godot 4

--- VMCControl/VMCBlendShapeControl.gd
+++ VMCControl/VMCBlendShapeControl.gd
@@ -10,8 +10,8 @@ class ExpressionData:
 
 class MorphData:
 	var mesh: MeshInstance3D
-	var morph: String
-	var values: Array
+	var blend_idx: int
+	var values: PackedFloat32Array
 
 var expression_data: Dictionary = {}
 
@@ -22,42 +22,57 @@ func __modify_blend_shape(mesh_instance: MeshInstance3D, blend_shape: String, va
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for animation_name in self.get_animation_list():
-		self.current_animation = animation_name
-		self.seek(0, true)
-	
-	for animation_name in self.get_animation_list():
-		expression_data[animation_name] = ExpressionData.new()
+		var expression: ExpressionData = ExpressionData.new()
 		var animation: Animation = self.get_animation(animation_name)
 		for track_index in animation.get_track_count():
 			var track_name: String = animation.track_get_path(track_index)
-			var split_name:  = track_name.split(":")
+			var split_name: PackedStringArray = track_name.split(":")
 			
 			if split_name.size() != 2:
 				print("Model has ultra nested meshes: %s" % track_name)
 				continue
 			
-			var mesh = get_node_or_null((split_name[0]))
-			if not mesh:
+			var mesh = get_node_or_null('../' + split_name[0])
+			if not mesh or not mesh is MeshInstance3D:
 				#print("Unable to find mesh: %s" % split_name[0])
 				continue
 			
 			var md = MorphData.new()
 			md.mesh = mesh
-			md.morph = split_name[1]
+			var blend_idx = mesh.find_blend_shape_by_name(split_name[1])
+			if blend_idx == null:
+				# Only save blend shape animation tracks
+				continue
+			
+			md.blend_idx = blend_idx
 			
 			for key_index in animation.track_get_key_count(track_index):
 				md.values.append(animation.track_get_key_value(track_index, key_index))
 			
-			expression_data[animation_name].morphs.append(md)
-
+			expression.morphs.append(md)
+			var vrm_blend_name: String = animation_name.split('/', true, 1)[1]
+			self.expression_data[vrm_blend_name] = expression
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# Reset all animations
+	for animation_name in self.get_animation_list():
+		self.current_animation = animation_name
+		self.seek(0, true)
+	
 	for name in self.vmc_receiver.blend_shapes:
-		if self.get_animation(name.to_upper()) != null:
-			var val: float = self.vmc_receiver.blend_shapes[name]
-			self.current_animation = name.to_upper()
-			self.seek(val, true)
-		else:
-			#print("Unknown blend shape: '" + name + "'")
-			pass
+		var expressions: ExpressionData = self.expression_data.get(name.to_upper(), null)
+		if not expressions:
+			continue
+		
+		for morph in expressions.morphs:
+			var anim_val: float = self.vmc_receiver.blend_shapes[name]
+			var track_count: int = morph.values.size()-1
+			var key_idx: int = ceil(anim_val*track_count)
+			var blend_val: float = morph.mesh.get_blend_shape_value(morph.blend_idx)
+			if key_idx != 0:
+				var weight: float = 1 - (key_idx/track_count - anim_val)
+				blend_val += float(lerpf(morph.values[key_idx-1], morph.values[key_idx], weight))
+				#blend_val += anim_val
+			
+			morph.mesh.set_blend_shape_value(morph.blend_idx, blend_val)
--- VMCControl/VMCBoneControl.gd
+++ VMCControl/VMCBoneControl.gd
@@ -4,33 +4,63 @@ extends Skeleton3D
 @export var vmc_receiver_path: NodePath = "/root/Main/VMCReceiver"
 @onready var vmc_receiver = get_node(self.vmc_receiver_path)
 
-var to_vmc_bone_mapping: Dictionary
+var vmc_to_bone_idx: Dictionary
+
+func __utg_01_convert(u: Vector3) -> Vector3:
+	return Vector3(u.x, -u.y, -u.z)
+
+func __utg_02l_convert(u: Vector3) -> Vector3:
+	return Vector3(u.z, -u.y, u.x)
+
+func __utg_02r_convert(u: Vector3) -> Vector3:
+	return Vector3(-u.z, -u.y, u.x)
+
+var __utg_converters: Dictionary = {
+	'hips': self.__utg_01_convert,
+	'spine': self.__utg_01_convert,
+	'chest': self.__utg_01_convert,
+	'upper_chest': self.__utg_01_convert,
+	'neck': self.__utg_01_convert,
+	'head': self.__utg_01_convert,
+	'leftupperarm': self.__utg_02l_convert,
+	'rightupperarm': self.__utg_02r_convert,
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Invert bone mapping
-	var bone_mapping: Dictionary = get_parent().vrm_meta.humanoid_bone_mapping
-	self.to_vmc_bone_mapping.clear()
-	for key in bone_mapping:
-		var vrm_name: String = key
-		self.to_vmc_bone_mapping[bone_mapping[key]] = vrm_name.substr(0,1).capitalize() + vrm_name.substr(1)
+	var bone_mapping: BoneMap = get_parent().vrm_meta.humanoid_bone_mapping
+	self.vmc_to_bone_idx.clear()
+	for i in range(0, bone_mapping.profile.bone_size):
+		var profile_bone_name: String = bone_mapping.profile.get_bone_name(i)
+		var skel_bone_name: String = bone_mapping.get_skeleton_bone_name(profile_bone_name)
+		if skel_bone_name:
+			var bone_data = [self.find_bone(profile_bone_name),
+				self.__utg_converters.get(profile_bone_name.to_lower(), self.__utg_01_convert)
+			]
+			self.vmc_to_bone_idx[profile_bone_name] = bone_data
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# TODO: Add bone stiffness and/or compare to vmc_receiver.t?
+	self.reset_bone_poses()
+	self.clear_bones_global_pose_override()
+	self.clear_bones_local_pose_override()
 	var bone_poses: Dictionary = self.vmc_receiver.bone_poses
-	for idx in range(0, self.get_bone_count()):
-		var bone_name: String = self.get_bone_name(idx)
-		var vrm_bone_name = self.to_vmc_bone_mapping.get(bone_name, null)
-		if vrm_bone_name:
-			var bone_pose = bone_poses.get(vrm_bone_name, null)
-			if bone_pose: # and bone_name == 'hips':
-				bone_pose.origin = Vector3.ZERO
-				# Convert from Unity to Godot coordinate system
-				# At the moment, we're only changing rotation and ignoring translation
-				var euler = bone_pose.basis.get_euler()
-				euler.x = -euler.x
-				euler.y = -euler.y
-				bone_pose.basis = Basis(euler)
-				self.set_bone_pose(idx, bone_pose)
-	
+	for bone_name in bone_poses:
+		var bone_data = self.vmc_to_bone_idx.get(bone_name, null)
+		#if bone_name.to_lower() != 'head' and \
+		#		bone_name.to_lower() != 'neck':
+		#	continue
+		if bone_data:
+			var bone_idx = bone_data[0]
+			var converter = bone_data[1]
+			var bone_pose = bone_poses[bone_name]
+			var rest_pose = self.get_bone_rest(bone_idx).basis.get_euler()
+			
+			# Convert from Unity to Godot coordinate system
+			# At the moment, we're only changing rotation and ignoring translation
+			var euler = bone_pose.basis.get_euler()
+			euler = converter.call(euler)
+			
+			self.set_bone_pose_rotation(bone_idx, Quaternion(rest_pose)*Quaternion(euler))
